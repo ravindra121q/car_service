e0b3aac239ddf8cb5149bd82a2ff70639d10ba10
ravi121q <ravindrakumartilotia4@gmail.com>
2023-10-17 10:07:09 +0530

    Added Header and Footer Homepage (Frontend)

--- frontend/src/routes/All_Routes.jsx
+++ frontend/src/routes/All_Routes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage";
+import { SignUpPage } from "../pages/SignUpPage";
 
 export const All_Routes = () => {
   return (
     <Routes>
       <Route path={"/"} element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
 };
